d8c09fda280bd27d51ff36db9d464fca6a73eb4b
Nsamijo <nsamijo@gmail.com>
2021-11-04 17:32:43 +0100

    Summary: Restructured routes
    Improvements;
    - app.jsx: Removed Routing
    - Navigation.js: Added Routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Row} from "react-bootstrap";
-import {Route, Switch} from "react-router-dom";
-import About from "./components/about/about";
 import Navigation from "./components/navigation/Navigation";
 
 class App extends React.Component {
@@ -11,11 +9,6 @@ class App extends React.Component {
                 <Row>
                     <Navigation/>
                 </Row>
-                <Switch>
-                  <Route path="/about">
-                    <About/>
-                  </Route>
-                </Switch>
             </div>
         )
     }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,27 +1,34 @@
 import React from "react";
 import {Container, Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Route, Router, Switch} from "react-router-dom";
+import About from "../about/about";
 
 class Navigation extends React.Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="/Stage">Portfolio Nathan K. Samijo</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <NavDropdown title="Leerdoelen" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Manage en Control</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Analyseren</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Adviseren</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Realiseren</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Professional Skills</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="/Stage/about">About</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <Router history={this.state.history}>
+                <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand href="/Stage">Portfolio Nathan K. Samijo</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <NavDropdown title="Leerdoelen" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Manage en Control</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Analyseren</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Adviseren</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Realiseren</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Professional Skills</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="/Stage/about">About</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <Switch>
+                    <Route path="/about" exact render={<About/>}/>
+                </Switch>
+            </Router>
         );
     }
 }
