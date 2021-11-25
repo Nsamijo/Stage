ffd745c5e75607e062372c32bef3de137b94a9bb
Nsamijo <nsamijo@gmail.com>
2021-11-25 13:40:26 +0100

    Summary: Changed router type
    Changes:
    - app.jsx: Changed BrowserRouter to HashRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navigation from "./components/navigation/Navigation";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {HashRouter as Router, Switch, Route} from "react-router-dom";
 import About from "./components/about/about";
 
 function App() {
