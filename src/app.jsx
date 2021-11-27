052bc38a9d08f03e47bdd654a523d4c5b7b9c1ec
Nsamijo <nsamijo@gmail.com>
2021-11-27 14:25:51 +0100

    Summary: Restructured components structure
    Changes:
    - app.jsx:
      - Removed Navigation code
      - Added Navigation Component
    - Navigation.js:
      - Added Routing for components
      - Added components path

--- src/app.jsx
+++ src/app.jsx
@@ -1,55 +1,12 @@
 import React from "react";
-import {HashRouter as Router, Route, Link} from "react-router-dom";
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import About from "./components/about/about";
-import Home from "./components/home/Home";
+import {HashRouter as Router} from "react-router-dom";
+import Navigation from "./components/navigation/Navigation";
 
 function App() {
     return (
         <Router>
             <div className="App">
-
-                {/*Navigation*/}
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="/Stage">Portfolio Nathan K. Samijo</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <NavDropdown title="Leerdoelen" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Manage en Control</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Analyseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Adviseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Realiseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Professional Skills</NavDropdown.Item>
-                                </NavDropdown>
-                                    <Nav.Link activeClassName="chosen">
-                                        <Link
-                                            to="/About"
-                                            style={{
-                                                color: 'inherit',
-                                                textDecoration: 'inherit',
-                                                display: 'contents'
-                                            }}
-                                        >
-                                            About
-                                        </Link>
-                                    </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                {/*Navigation*/}
-                <Container>
-                    <div className="Content">
-                        <Route exact path="/">
-                            <Home/>
-                        </Route>
-                        <Route exact path="/About">
-                            <About/>
-                        </Route>
-                    </div>
-                </Container>
+                <Navigation/>
             </div>
         </Router>
     )
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,27 +1,54 @@
 import React from "react";
 import {Container, Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Link, Route} from "react-router-dom";
+import Home from "../home/Home";
+import About from "../about/about";
 
 class Navigation extends React.Component {
     render() {
         return (
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="/Stage">Portfolio Nathan K. Samijo</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <NavDropdown title="Leerdoelen" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Manage en Control</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Analyseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Adviseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Realiseren</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Professional Skills</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href="/Stage/About" activeClassName="chosen">About</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+            <>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/Stage">Portfolio Nathan K. Samijo</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavDropdown title="Leerdoelen" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Manage en Control</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Analyseren</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Adviseren</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Realiseren</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Professional Skills</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link activeClassName="chosen">
+                                <Link
+                                    to="/About"
+                                    style={{
+                                        color: 'inherit',
+                                        textDecoration: 'inherit',
+                                        display: 'contents'
+                                    }}
+                                >
+                                    About
+                                </Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        {/*Navigation*/}
+            <Container>
+                <div className="Content">
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route exact path="/About">
+                        <About/>
+                    </Route>
+                </div>
+            </Container>
+            </>
         );
     }
 }
