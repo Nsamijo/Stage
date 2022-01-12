86f745a8d0cf0dcb1b6b22487611e6cdf4408ff0
Nsamijo <nsamijo@gmail.com>
2022-01-12 04:37:46 +0100

    implemented react-bootstrap

--- src/components/Leerdoelen/leerdoelen.js
+++ src/components/Leerdoelen/leerdoelen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Button, Carousel, CarouselItem, Col, Nav, Row} from "react-bootstrap";
 
 import "./leerdoelen.css";
 
@@ -7,35 +7,53 @@ import "./leerdoelen.css";
 const Leerdoelen = () => {
     return (
         <Row>
-            <Col>
-                <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#leerdoelen" role="button" data-slide-to="0">1</a>
-                    </li>
-                    <li className="nav-item" >
-                        <a className="nav-link" href="#leerdoelen" role="button" data-slide-to="1">2</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#leerdoelen" role="button" data-slide-to="2">3</a>
-                    </li>
-                </ul>
+            <Col sm={12} lg={3}>
+                <Nav variant="pills" className="flex-column" defaultActiveKey="manage">
+                    <Nav.Item>
+                        <Nav.Link eventKey="manage" href="#leerdoelen" data-slide-to="0">Manage and Control</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item  >
+                        <Nav.Link eventKey="analyse" href="#leerdoelen" data-slide-to="1">Analyseren</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item >
+                        <Nav.Link eventKey="advies" href="#leerdoelen"  data-slide-to="2">Adviseren</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item >
+                        <Nav.Link eventKey="ontwerp" href="#leerdoelen"  data-slide-to="3">Ontwerpen</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item >
+                        <Nav.Link eventKey="realise" href="#leerdoelen"  data-slide-to="4">Realiseren</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item >
+                        <Nav.Link eventKey="skills" href="#leerdoelen"  data-slide-to="5">Professional Skills</Nav.Link>
+                    </Nav.Item>
+                </Nav>
             </Col>
-            <Col>
-                <div id="leerdoelen" className="carousel slide" data-interval={false} data-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
+            <Col sm={12} lg={9}>
+                <Carousel id="leerdoelen" interval={null} controls={false} indicators={false}>
+                        <CarouselItem>
                             <span>
                                 Hello World
                             </span>
-                        </div>
-                        <div className="carousel-item">
+                        </CarouselItem>
+                        <CarouselItem>
                             <img className="d-block w-100" src="..." alt="Second slide"/>
-                        </div>
-                        <div className="carousel-item">
+                        </CarouselItem>
+                        <CarouselItem>
                             <img className="d-block w-100" src="..." alt="Third slide"/>
-                        </div>
-                    </div>
-                </div>
+                        </CarouselItem>
+                    <CarouselItem>
+                            <span>
+                                Hello World
+                            </span>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <img className="d-block w-100" src="..." alt="Second slide"/>
+                    </CarouselItem>
+                    <CarouselItem>
+                        <img className="d-block w-100" src="..." alt="Third slide"/>
+                    </CarouselItem>
+                </Carousel>
             </Col>
         </Row>
     )
