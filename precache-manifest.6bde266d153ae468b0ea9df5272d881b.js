b49727a10c29b9bd65c46f922c14f7243d3c6f70
Nsamijo <43448925+Nsamijo@users.noreply.github.com>
2022-07-12 12:21:42 +0000

    Deploying to gh-pages from @ Nsamijo/Stage@bbdfe99da9fb42ddc98c26b3a30b4a39ec47f3f7 🚀

---
+++ precache-manifest.6bde266d153ae468b0ea9df5272d881b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f80c4e1d91eb988b56c",
+    "url": "/Stage/static/css/main.34b43aac.chunk.css"
+  },
+  {
+    "revision": "7f80c4e1d91eb988b56c",
+    "url": "/Stage/static/js/main.7f80c4e1.chunk.js"
+  },
+  {
+    "revision": "5b84e86cee59c48b96e9",
+    "url": "/Stage/static/css/1.9010b591.chunk.css"
+  },
+  {
+    "revision": "5b84e86cee59c48b96e9",
+    "url": "/Stage/static/js/1.5b84e86c.chunk.js"
+  },
+  {
+    "revision": "f0c3f8a6f635ea11c41e",
+    "url": "/Stage/static/js/runtime~main.f0c3f8a6.js"
+  },
+  {
+    "revision": "c35662a3394e55ccc2b65ca7699d30e8",
+    "url": "/Stage/static/media/criteria.c35662a3.png"
+  },
+  {
+    "revision": "f94609f16d9b6c64d66732d513f04b9c",
+    "url": "/Stage/static/media/pasfoto.f94609f1.png"
+  },
+  {
+    "revision": "c8a7dce952b6c44ceb25307ed022febf",
+    "url": "/Stage/static/media/Prijs.c8a7dce9.png"
+  },
+  {
+    "revision": "bbc94f4d9a47cdccd5c63590803257e5",
+    "url": "/Stage/static/media/toewijzing.bbc94f4d.png"
+  },
+  {
+    "revision": "2140a1ab55ef28a73786dd02a7a42284",
+    "url": "/Stage/static/media/gereed.2140a1ab.png"
+  },
+  {
+    "revision": "a59ce2ef7f05c438a9e2a0eb1625b1ac",
+    "url": "/Stage/static/media/resultaat.a59ce2ef.png"
+  },
+  {
+    "revision": "ff21fcf8bd418a17544b44246e07e3fd",
+    "url": "/Stage/static/media/pr.ff21fcf8.png"
+  },
+  {
+    "revision": "e38ab6c697d2796f76fd95d70d9e814d",
+    "url": "/Stage/static/media/github-actions.e38ab6c6.png"
+  },
+  {
+    "revision": "70445f31c8262afc9ccfd336740fbb90",
+    "url": "/Stage/static/media/allproducts.70445f31.png"
+  },
+  {
+    "revision": "17865c335dc976583906e2530469fc57",
+    "url": "/Stage/static/media/product.17865c33.png"
+  },
+  {
+    "revision": "81b0f7045e8827ba6bf8e3a7110e3e66",
+    "url": "/Stage/static/media/pr.81b0f704.png"
+  },
+  {
+    "revision": "218ca307b9a5162c8bb38fdac8b3d6e5",
+    "url": "/Stage/static/media/added.218ca307.png"
+  },
+  {
+    "revision": "aa7694739e191dfe3dd46e825c75b581",
+    "url": "/Stage/static/media/index.aa769473.png"
+  },
+  {
+    "revision": "ebed273fe98457128b215a97415d4e50",
+    "url": "/Stage/static/media/styling.ebed273f.png"
+  },
+  {
+    "revision": "6124970d0392f15c091c0b8bee2ecaec",
+    "url": "/Stage/static/media/team.6124970d.png"
+  },
+  {
+    "revision": "cff51854fbb394a46766ef5ddd35f269",
+    "url": "/Stage/index.html"
+  }
+];
